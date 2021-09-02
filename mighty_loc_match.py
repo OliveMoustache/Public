a70cce389f238e52a07f41b0773508e48308f18f
OliveMoustache <59829223+OliveMoustache@users.noreply.github.com>
2021-09-02 16:07:24 +0200

    Update mighty_loc_match.py
    added the Xform command in comment.

--- mighty_loc_match.py
+++ mighty_loc_match.py
@@ -6,3 +6,20 @@ locName =  posGrab[0] + '_Loc'
 cmds.spaceLocator ( n =  locName  )
 cmds.parent( locName,posGrab, r = 1 )
 cmds.parent( locName, w =1 )
+
+
+## Second method with Xform
+'''
+## Locator match position with Xform command
+## Create a locator with a name based on the object and snap it to the position of that object
+import maya.cmds as cmds
+posGrab = cmds.ls(selection=True)
+locName =  posGrab[0] + '_Loc' 
+cmds.spaceLocator ( n =  locName  )
+
+clusterPos = cmds.xform( posGrab, query=True, translation=True, worldSpace=True )
+clusterRot = cmds.xform( posGrab, query=True, rotation=True, worldSpace=True )
+cmds.xform ( locName, t= (clusterPos[0],clusterPos[1], clusterPos[2]) )
+cmds.xform ( locName, ro= (clusterRot[0],clusterRot[1], clusterRot[2]) )
+
+'''
