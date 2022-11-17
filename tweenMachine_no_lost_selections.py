a180d5ffbe778a31a88664c4993c528dba5f5a99
OliveMoustache <59829223+OliveMoustache@users.noreply.github.com>
2022-11-17 15:08:00 -0500

    Update tweenMachine_no_lost_selections.py

--- tweenMachine_no_lost_selections.py
+++ tweenMachine_no_lost_selections.py
@@ -1,3 +1,6 @@
+// simple command to save and reselect your current selection 
+// after launching TweenMachine
+
 ctlSel = cmds.ls( selection=True )
 mel.eval('tweenMachine()')
 cmds.select ( ctlSel )
