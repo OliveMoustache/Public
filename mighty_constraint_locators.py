b72ecfeb7625654ec0144480b24bd4e5729ac170
OliveMoustache <59829223+OliveMoustache@users.noreply.github.com>
2022-12-14 09:51:32 -0500

    Update mighty_constraint_locators.py

--- mighty_constraint_locators.py
+++ mighty_constraint_locators.py
@@ -1,6 +1,11 @@
 ##### Procedure to constraint a locator to all the selected controls
 #### You might want to customize section 1 to group your constraints where required by your pipeline
 
+#### TODO
+#### Naming of the constraints doesn't work properly => issue for mighty_locators_to_CTRLs.py
+### seems to be a problem with the namespace, works fine without Namespace
+
+
 import maya.cmds as cmds 
 
 ## make a list from the selection
@@ -18,9 +23,10 @@ for i in range(len(sel)):
 
     mightyLocator = 'mighty_Loc_' + sel[i]
     print ( 'Locator creation for ' + mightyLocator )
-    
+    #### creation of the locator
     cmds.spaceLocator ( name = mightyLocator ) 
-    cmds.parentConstraint ( sel[i], mightyLocator, name = 'mighty_constraint_temp_parentConstraint_' + sel[i] )
+    #### constraint of the locator to the control
+    cmds.parentConstraint ( sel[i], mightyLocator, name = 'mighty_constraint_temp_parentConstraint__' + sel[i] )
 
     #### Section 1
     #### This is where you would customize the script and group the locators to your Sandbox or Temp group
